c5b1469da3582a7be7035276e272cf0585984f9f
Carson Brownlee <carson@login1.maverick.tacc.utexas.edu>
2016-01-13 18:03:19 -0600

    cleaning up unecessary binary files

--- benchmarks/interactive/inter_ddns_clipsweep_osp_rgpu_t5_n1.sh
+++
@@ -1,1 +0,0 @@
-/work/00401/pnav/workloads/svb_st/benchmarks/submits/submit_ddns_clipsweep_osp_rgpu_t5_n1.sh | 2>&1 tee /work/00401/pnav/workloads/svb_st/benchmarks/outs/ddns_clipsweep_osp_rgpu_t5_n1.out
--- benchmarks/interactive/inter_ddns_clipsweep_rvbo_t1_n1.sh
+++
@@ -1,1 +0,0 @@
-/work/00401/pnav/workloads/svb_st/benchmarks/submits/submit_ddns_clipsweep_rvbo_t1_n1.sh | 2>&1 tee /work/00401/pnav/workloads/svb_st/benchmarks/outs/ddns_clipsweep_rvbo_t1_n1.out
